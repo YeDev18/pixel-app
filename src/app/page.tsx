b5338a41f52e5401b795472d4b1c52abf0984ec1
Emmanuel252 <ouattarayoussoufemmanuel@gmail.com>
2024-10-15 13:37:48 +0000

    setup: Initialize of colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <div className=" flex items-center justify-center w-screen h-screen  font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-3xl font-bold">Pixel app</h1>
+    <div className=" flex items-center justify-center w-screen bg-blue-800 h-screen">
+      <h1 className="text-3xl font-bold text-light-700">Pixel app</h1>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,14 +3,21 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/core/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        red: {
+          850: '#dc0000'
+        },
+      blue:{
+        800:'#0a2b38'
+      },
+      light:{
+        700:"#fae3cf"
+      }
       },
     },
   },
