19a33a528b6ee9235338f4d8b6199dd3e89db8a5
ArmelK08 <koblankossonou@gmail.com>
2024-10-16 09:58:13 +0000

    bgColor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,5 +4,6 @@ export default function Home() {
     <div className=" flex items-center justify-center w-screen h-screen  font-[family-name:var(--font-geist-sans)]">
       <h1 className="text-3xl font-bold">Pixel app</h1>
     </div>
+   
   );
 }
---
+++ src/core/data/service.data.tsx
@@ -0,0 +1,61 @@
+// import arrowLink from '@/app/img/arrow-link.svg';
+import BrandDesign from '@/app/img/BrandDesignStrategy.svg';
+import googleAds from '@/app/img/GoogleAds.svg';
+import marketing from '@/app/img/MarketingConsulting.svg';
+import sales from '@/app/img/SalesMarketingDepartmentAudit.svg';
+import salesScratch from '@/app/img/SalesDepartmentsFromScratch.svg';
+import smm from '@/app/img/SMM.svg';
+import web from '@/app/img/WebDevelopment.svg';
+import seo from '@/app/img/SEO.svg';
+import email from '@/app/img/EmailMarketing.svg';
+// import extention from '@/app/img/Extention.png'
+
+const svgData = [
+  {
+    id: 1,
+    name: "Brand Design & Strategy",
+    svg: BrandDesign,
+  },
+  {
+    id: 2,
+    name: "Google Ads",
+    svg: googleAds,
+  },
+  {
+    id: 3,
+    name: "Marketing Consulting",
+    svg: marketing,
+  },
+  {
+    id: 4,
+    name: "Sales & Marketing Department Audit",
+    svg: sales,
+  },
+  {
+    id: 5,
+    name: "Sales Departments from Scratch",
+    svg: salesScratch,
+  },
+  {
+    id: 6,
+    name: "Social Media Marketing",
+    svg: smm,
+  },
+  {
+    id: 7,
+    name: "Web Development",
+    svg: web,
+  },
+  {
+    id: 8,
+    name: "SEO",
+    svg: seo,
+  },
+  {
+    id: 9,
+    name: "Email Marketing",
+    svg: email,
+  },
+];
+
+export default svgData;
