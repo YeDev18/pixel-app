71bebb6349a456f9932d221d6d7997fd4e0a2515
ArmelK08 <koblankossonou@gmail.com>
2024-11-20 15:06:25 +0000

    form

--- src/core/components/molecules/contact/form/form.tsx
+++ src/core/components/molecules/contact/form/form.tsx
@@ -3,11 +3,11 @@ import Input from "@/core/components/atoms/input";
 const FormField = () => {
 	return (
 		<>
-			<div className="flex w-full flex-col  items-center justify-center gap-6 rounded-3xl bg-blue-800 py-16 lg:h-[45rem] lg:w-[50rem]">
+			<div className="flex w-full h-f ull flex-col  items-center justify-center gap-4 rounded-3xl bg-blue-800 py-16 lg:h-[45rem] lg:w-[50rem]">
 				<h4 className="overflow-hidden p-4 text-4xl font-extralight text-light-100">
 					Contactez-Nous
 				</h4>
-				<div className="flex w-2/4 flex-col gap-8 max-md:w-full max-md:px-4 lg:w-3/4">
+				<div className="flex w-2/4 flex-col gap-6 max-md:w-full max-md:px-4 lg:w-3/4">
 					<Input
 						outline-none
 						border-none
@@ -16,14 +16,26 @@ const FormField = () => {
 						placeholder="Nom"
 					/>
 					<Input
+						outline-none
+						border-none
+						className=" w-full border-none bg-[#04374b] outline-none"
+						type="Number"
+						placeholder="Numéro De Téléphone"
+					/>
+					<Input
 						className=" w-full border-none bg-[#04374b] outline-none"
 						type="email"
 						placeholder="Email"
 					/>
+					<Input
+						className=" w-full border-none bg-[#04374b] outline-none"
+						type="text"
+						placeholder="Secteur D'activité"
+					/>
 					<div className="h-56 max-h-60 w-full overflow-hidden  py-2 lg:px-1">
 						<textarea
 							className="h-52 max-h-52 min-h-16 w-full rounded-[25px] bg-[#04374b] p-3  text-white shadow-inner outline-none"
-							placeholder="Email"
+							placeholder="Dites Nous Vos Besoins"
 						></textarea>
 					</div>
 
