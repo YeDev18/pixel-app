57289edbb2c6989fef9cf09f784482c0eefec9e6
Emmanuel252 <ouattarayoussoufemmanuel@gmail.com>
2024-11-22 00:31:37 +0000

    setup: Update word

--- src/core/components/molecules/service.item/card/card.tsx
+++ src/core/components/molecules/service.item/card/card.tsx
@@ -1,14 +1,14 @@
+import { Computer } from "@/core/components/atoms/icons/service.icon";
+
 const Card = () => {
 	return (
-		<div>
-			<span>Lorem ipsum dolor sit amet consectetur.</span>
-			<span>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore
-				aut veritatis reiciendis in illum ipsa nihil sunt fuga aliquam
-				itaque nam culpa consectetur error, necessitatibus saepe
-				inventore omnis alias quos.
+		<div className="flex w-[35rem] items-start  gap-2">
+			<span className="shrink-0">
+				<Computer />
 			</span>
-			<span>
+
+			<span className="cardText mr-16 text-wrap ">
+				<span>Lorem ipsum </span>
 				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore
 				aut veritatis reiciendis in illum ipsa nihil sunt fuga aliquam
 				itaque nam culpa consectetur error, necessitatibus saepe
