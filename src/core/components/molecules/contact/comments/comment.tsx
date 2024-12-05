73cf38162494e7260e0023c09a219bce60093dc5
Emmanuel252 <ouattarayoussoufemmanuel@gmail.com>
2024-12-05 23:58:33 +0000

    setup: contact

--- src/core/components/molecules/contact/comments/comment.tsx
+++ src/core/components/molecules/contact/comments/comment.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import ArrowUp from "@/core/components/atoms/icons/arrow.up";
 import Quote from "@/core/components/atoms/icons/quote";
 import Avatar from "@/core/components/atoms/icons/user";
@@ -19,24 +19,30 @@ const Comments = () => {
 	};
 	return (
 		<>
-			<div className="relative h-full w-full ">
+			<div className="relative w-full bg-black ">
 				<div className="absolute bottom-14 right-14 flex flex-col space-y-4 text-2xl font-semibold text-light-100">
-					<button className=" cursor-pointer rounded-full border p-2">
+					<button
+						onClick={() => prevIndex}
+						className=" cursor-pointer rounded-full border p-2"
+					>
 						<ArrowUp />
 					</button>
-					<button className="rotate-180 cursor-pointer  rounded-full border p-2 ">
+					<button
+						onClick={() => nextIndex}
+						className="rotate-180 cursor-pointer  rounded-full border p-2 "
+					>
 						<ArrowUp />
 					</button>
 				</div>
 				{CommmentsData.map((comment) => {
-					if (comment.id === 4) {
+					if (comment.id === commentIndex) {
 						return (
 							<div
 								className="w-full text-light-100"
 								key={comment.id}
 							>
 								<div className="flex h-[45rem] max-h-[48rem] w-full  flex-col justify-between rounded-3xl bg-blue-800 p-8  lg:w-full">
-									<Quote color="#DC0000"/>
+									<Quote color="#DC0000" />
 									<p className="text-xl/relaxed font-medium lg:text-2xl/relaxed">
 										{comment.text}
 									</p>
